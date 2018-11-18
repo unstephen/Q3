c0d53e60e9424d3cbf1c2010627709180592f8e9
simaHasaki <zyz1991910@sina.com>
2018-11-19 00:45:58 +0800

    1.fixed;

--- Assets/GameMain/Scripts/NetWork/MsgObject.cs
+++ Assets/GameMain/Scripts/NetWork/MsgObject.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
-public class MsgBase
+/// <summary>
+/// 接受的基础数据
+/// </summary>
+public class Recv_MsgBase
 {
     /// <summary>
     /// 错误码
@@ -15,49 +19,117 @@ public class MsgBase
 /// <summary>
 /// 进入大厅
 /// </summary>
-public class Get_MainPage : MsgBase
+public class Recv_Get_MainPage : Recv_MsgBase
 {
-
+    public string name;
+    public string image_url;
+    public uint balance; //账户余额
 }
 /// <summary>
 /// 商城
 /// </summary>
-public class Get_Shop : MsgBase
+public struct GoodsData
 {
+    public int goodsId;
+    public int goodsType;
+    public int num;
+    public float price;
+}
 
+public class Recv_Get_Shop : Recv_MsgBase
+{
+    public int goodsCount;
+    public List<GoodsData> goodsList;
 }
 /// <summary>
 /// 购买物品
 /// </summary>
-public class Post_PurchaseItem : MsgBase
+public class Recv_Post_PurchaseItem : Recv_MsgBase
+{
+    public int appId;
+    public int merchantId;
+    public int orderId;
+    public string randomStr;
+    public string sign;
+}
+/// <summary>
+/// 我的俱乐部
+/// </summary>
+public class Recv_Get_MyClub : Recv_MsgBase
 {
 
 }
 /// <summary>
-/// 俱乐部
+/// 俱乐部详情
 /// </summary>
-public class Get_Club : MsgBase
+public class Recv_Get_ClubInfo : Recv_MsgBase
 {
 
 }
 /// <summary>
 /// 创建俱乐部
 /// </summary>
-public class Post_CreatClub : MsgBase
+public class Recv_Post_CreatClub : Recv_MsgBase
 {
 
 }
 /// <summary>
 /// 搜索俱乐部
 /// </summary>
-public class Get_SearchClub : MsgBase
+public class Recv_Get_SearchClub : Recv_MsgBase
 {
 
 }
 /// <summary>
 /// 申请俱乐部
 /// </summary>
-public class Post_ApplyClub : MsgBase
+public class Recv_Post_ApplyClub : Recv_MsgBase
+{
+
+}
+/// <summary>
+/// 处理俱乐部申请
+/// </summary>
+public class Recv_Post_HandleRequest : Recv_MsgBase
+{
+
+}
+/// <summary>
+/// 历史战绩（总览）
+/// </summary>
+public class Recv_Get_History : Recv_MsgBase
+{
+
+}
+/// <summary>
+/// 查询历史战绩
+/// </summary>
+public class Recv_Get_SearchHistory : Recv_MsgBase
+{
+
+}
+
+/// <summary>
+/// 发送的基础数据
+/// </summary>
+public class Send_MsgBase
+{
+    public int userId;
+    public string access_token;
+    public double linuxTimes;
+}
+
+public class Send_Get_MainPage : Send_MsgBase
+{
+    
+}
+
+public class Send_Get_Shop : Send_MsgBase
+{
+
+}
+
+public class Send_Post_PurchaseItem : Send_MsgBase
 {
 
 }
--- Assets/GameMain/Scripts/ScriptPublic/GameConst.cs
+++ Assets/GameMain/Scripts/ScriptPublic/GameConst.cs
@@ -8,4 +8,16 @@ public class GameConst {
     public const int pokerMax = 13;
 
     public const string httpUrl = "http://www.baidu.com:8080";
+
+    public string _mainPage = "/user/info";
+    public string _shop = "/goods/list";
+    public string _purchaseItem = " /goods/pay";
+    public string _myClub = "/club/list";
+    public string _clubInfo = "/club/info";
+    public string _createClub = "/club/create";
+    public string _searchClub = "/club/search";
+    public string _applyClub = "/club/apply";
+    public string _handleRequest = "/apply/deal";
+    public string _history = "/history/overview";
+    public string _searchHistory = "/history/list";
 }
